26b304da3b2706d49c5313de676f6cab889a22bc
WoelliA <andreas.woelfl@stud.uni-regensburg.de>
2016-07-12 23:11:08 +0200

    marker Setup

--- src/wiimotePositionMapper.py
+++ src/wiimotePositionMapper.py
@@ -1,6 +1,21 @@
-
 class WiiMotePositionMapper(object):
-    markers = []
+    def markers(self):
+
+        markerLeftBottom = (0, 0)
+        markerRightBottom = (1024, 0)
+        markerLeftTop = (1024, 768)
+        markerRigthTop = (0, 768)
+        interactionRec = [(markerLeftBottom), (markerRightBottom), (markerRigthTop), (markerRigthTop)]
+
+    # xlim(0, SRC_W)
+    # ylim(0, SRC_H)
+    # scatter(*zip(*scoords))  # repack points[] to axes[] for plotting
+    # scatter([SRC_W / 2], [SRC_H / 2], c='r', marker='+')
+
+
+
+
+
 
     def __init__(self):
         pass
