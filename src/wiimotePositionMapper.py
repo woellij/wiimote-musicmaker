44ca606b2d8b907f2bdfdedaafab1024f4c53186
WoelliA <andreas.woelfl@stud.uni-regensburg.de>
2016-07-13 00:05:29 +0200

    marker Setup 1,2

--- src/wiimotePositionMapper.py
+++ src/wiimotePositionMapper.py
@@ -1,23 +1,14 @@
+from numpy import *
+from scipy.linalg import solve
+
+
 class WiiMotePositionMapper(object):
-    def markers(self):
-
-        markerLeftBottom = (0, 0)
-        markerRightBottom = (1024, 0)
-        markerLeftTop = (1024, 768)
-        markerRigthTop = (0, 768)
-        interactionRec = [(markerLeftBottom), (markerRightBottom), (markerRigthTop), (markerRigthTop)]
-
-
+    markers = []
 
     # xlim(0, SRC_W)
     # ylim(0, SRC_H)
-    # scatter(*zip(*scoords))  # repack points[] to axes[] for plotting
+    # scatter(*zip(*scoords))  # repack points[] to axes[] for plottingd
     # scatter([SRC_W / 2], [SRC_H / 2], c='r', marker='+')
-
-
-
-
-
 
     def __init__(self):
         pass
@@ -25,3 +16,13 @@ class WiiMotePositionMapper(object):
     def map(self, data):
         # TODO
         return (0, 0)
+
+    def do(self, sx1, sx2, sx3, sy1, sy2, sy3, sx4, sy4):
+        # Step 1
+        source_points_123 = matrix([[sx1, sx2, sx3],
+                                    [sy1, sy2, sy3],
+                                    [1, 1, 1]])
+        source_point_4 = [[sx4],
+                          [sy4],
+                          [1]]
+        scale_to_source = solve(source_points_123, source_point_4)
