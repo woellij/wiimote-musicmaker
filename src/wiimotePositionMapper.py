dc06209616314d8383cfe0bb87e96bdd73a233b3
woellij <woellij@hotmail.de>
2016-07-14 09:29:56 +0200

    added euclidean distance ir marker sorting

--- src/wiimotePositionMapper.py
+++ src/wiimotePositionMapper.py
@@ -1,7 +1,9 @@
 from matplotlib.pyplot import xlim, ylim
 from numpy import *
+import numpy as np
 from scipy.linalg import solve, inv
 
+from operator import itemgetter
 
 class WiiMotePositionMapper(object):
     markers = []
@@ -18,14 +20,18 @@ class WiiMotePositionMapper(object):
         if len(data) < 4 or len(data) > 4:
             return None
 
-        ms = list(map(lambda d: (d["x"], d["y"]), data))
+        irDots = list(map(lambda d: (d["x"], d["y"]), data))
 
 
-        ms = sort(ms,0,)
-        print(ms)
+        irDots = sort(irDots,0,)
+        print(irDots)
         try:
-            res = self.do(*ms[0], *ms[1], *ms[2], *ms[3], WiiMotePositionMapper.DEST_W, WiiMotePositionMapper.DEST_H )
-            #res = self.do(ms[0][0], ms[0][1], ms[1][0], ms[1][1], ms[2][0], ms[2][1], ms[3][0], ms[3][1], )
+            irDots = self.orderIrDots(irDots)
+            irDots = self.filterMarkers(irDots)
+            if len(irDots) < 4 or len(irDots) > 4:
+                return None
+            res = self.do(*irDots[0], *irDots[1], *irDots[2], *irDots[3], WiiMotePositionMapper.DEST_W, WiiMotePositionMapper.DEST_H )
+            #res = self.do(irDots[0][0], irDots[0][1], irDots[1][0], irDots[1][1], irDots[2][0], irDots[2][1], irDots[3][0], irDots[3][1], )
             print(res)
 
 
@@ -34,8 +40,17 @@ class WiiMotePositionMapper(object):
             pass
         return (0, 0)
 
-    def filterMarkers(self, data):
-        markerDistance = ""
+    def orderIrDots(self, irDots):
+        xSorted = sorted(irDots, key=itemgetter(0))
+        bottomLeft = min(xSorted, axis=1)
+
+        withDistance =  map( lambda p: (p, np.linalg.norm(bottomLeft - p)), xSorted[1:])
+        distSorted = np.sort(withDistance, 0)
+
+        return [bottomLeft, distSorted[1], distSorted[3], distSorted[0]]
+
+    def filterMarkers(self, irDots):
+        return irDots
 
 
     def do(self, sx1, sy1, sx2, sy2, sx3, sy3, sx4, sy4, DEST_W, DEST_H):
